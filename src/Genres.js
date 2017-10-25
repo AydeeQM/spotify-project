c193bf84bdc8a3513cc96304bef68e70c8ef41a1
aydee <aydee.qm@gmail.com>
2017-10-25 09:47:34 -0500

    componiendo ruteo

--- src/Genres.js
+++ src/Genres.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
+import {NavLink} from 'react-router-dom';
 import {connect} from 'redux-zero/react'
 import './App.css';
 
 
 const Genres = ({report}) => {
    return (
-      <div>
+      <div className="bienvenida text-center">
+        <header>
+            <ul className="main-nav">
+                <li><NavLink to={"/feautures"} exact path="/">Feautures</NavLink></li>
+                <li><NavLink to={"/genres"}>Genres music</NavLink></li> 
+                <li><NavLink to={"/filtro"}>Filtro</NavLink></li>       
+            </ul>
+        </header>
          Genres
       </div>
    );
