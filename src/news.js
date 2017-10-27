c2e8b61e753574b3a79796b87653a8dc44985e69
maribelvm <m.velasquezmoscoso@gmail.com>
2017-10-27 01:43:46 -0700

    todos los archivos

--- src/news.js
+++ src/news.js
@@ -5,36 +5,36 @@ import './App.css';
 
 
 const News = ({ playNews }) => {
-    return (
-        <div className="bienvenida text-center">
-            <header>
-                <ul className="main-nav">
-                    <li><NavLink to={"/feautures"} exact path="/">Feautures</NavLink></li>
-                    <li><NavLink to={"/genres"}>Genres music</NavLink></li>
-                    <li><NavLink to={"/news"}>News</NavLink></li>
-                    <li><NavLink to={"/filtro"}>Filtro</NavLink></li>
-                </ul>
-            </header>
-            <section>
-                <h1>Nuevos álbumes y sencillos</h1>
+	return (
+		<div className="bienvenida text-center">
+			<header>
+				<ul className="main-nav">
+					<li><NavLink to={"/feautures"} exact path="/">Feautures</NavLink></li>
+					<li><NavLink to={"/genres"}>Genres music</NavLink></li>
+					<li><NavLink to={"/news"}>News</NavLink></li>
+					<li><NavLink to={"/filtro"}>Filtro</NavLink></li>
+				</ul>
+			</header>
+			<section>
+				<h1>Nuevos álbumes y sencillos</h1>
 
-                {
-                    playNews.map(playNews => {
-                        return (
-                            <div className="col-md-2 playlist">
-                                <img src={playNews.image} width="150" height="150" />
-                                <audio src={playNews.audio} controls ></audio>
-                                <div>
-                                   <h4>{playNews.title}</h4> 
-                                   <p>{playNews.artist}</p>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
-            </section>
-        </div>
-    );
+				{
+					playNews.map(playNews => {
+						return (
+							<div className="col-md-2 playlist">
+								<img src={playNews.image} width="150" height="150" />
+								<audio src={playNews.audio} controls ></audio>
+								<div>
+									<h4>{playNews.title}</h4>
+									<p>{playNews.artist}</p>
+								</div>
+							</div>
+						);
+					})
+				}
+			</section>
+		</div>
+	);
 }
 
 const mapToProps = ({ playNews }) => ({ playNews })
